d41003a5b5c4d05f1ca81c71006923e5c7a8464e
WilfriedB <WilfriedB@users.noreply.github.com>
2021-12-30 16:54:23 +0100

    Initial commit - a working analyzer

---
+++ src/DiscardedTaskAnalyzer/DiscardedTaskAnalyzer.Test/TestSubject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscardedTaskAnalyzer.Test
+{
+    internal class TestSubject
+    {
+        public Task TestMethod()
+        {
+            return Task.CompletedTask;
+        }
+
+        async public Task TestMethodWithAwait()
+        {
+            await Task.CompletedTask;
+        }
+
+        public void MethodWithCodeToAnalyze()
+        {
+            var t = new TestSubject();
+            _ = t.TestMethod();
+        }
+
+    }
+}
