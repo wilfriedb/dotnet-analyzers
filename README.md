b5576afbecf83eb274dfd80a1eedaf55c10a5b1d
WilfriedB <WilfriedB@users.noreply.github.com>
2023-03-04 12:37:48 +0100

    Updates for NuGet package

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Discarded Task Analyzer
+
+Icon: <a href="https://www.flaticon.com/free-icons/syntax" title="syntax icons">Syntax icons created by alfanz - Flaticon</a>
--- src/Cursoriam.Analyzers.Test/TestSubject.cs
+++ src/Cursoriam.Analyzers.Test/TestSubject.cs
@@ -1,28 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace Cursoriam.Analyzers.Test
+namespace Cursoriam.Analyzers.Test;
+
+internal class TestSubject
 {
-    internal class TestSubject
+    public Task TestMethod()
     {
-        public Task TestMethod()
-        {
-            return Task.CompletedTask;
-        }
-
-        async public Task TestMethodWithAwait()
-        {
-            await Task.CompletedTask;
-        }
-
-        public void MethodWithCodeToAnalyze()
-        {
-            var t = new TestSubject();
-            _ = t.TestMethod();
-        }
-
+        return Task.CompletedTask;
     }
+
+    async public Task TestMethodWithAwait()
+    {
+        await Task.CompletedTask;
+    }
+
+    public void MethodWithCodeToAnalyze()
+    {
+        var t = new TestSubject();
+        _ = t.TestMethod();
+    }
+
 }
